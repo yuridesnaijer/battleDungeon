216afec124426bc9eef379782046c76ac73c66a3
yuri <yuridesnaijer@gmail.com>
2017-03-09 11:59:39 +0100

    init

---
+++ core/src/com/morgenmiddag/game/Game.java
@@ -0,0 +1,84 @@
+package com.morgenmiddag.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.morgenmiddag.game.screens.MainMenuScreen;
+//import com.morgenmiddag.game.screens.PlayScreen;
+
+import com.badlogic.gdx.graphics.Color;
+import com.morgenmiddag.game.screens.SplashScreen;
+import com.morgenmiddag.game.screens.LoadingScreen;
+
+public class Game extends com.badlogic.gdx.Game {
+
+	public static final String TITLE = "Slidez";
+	public static final float VERSION = .8f;
+	public static final int V_WIDTH = 480;
+	public static final int V_HEIGHT = 420;
+
+	public OrthographicCamera camera;
+	public SpriteBatch batch;
+
+	public BitmapFont font24;
+
+	public AssetManager assets;
+
+	public LoadingScreen loadingScreen;
+	public SplashScreen splashScreen;
+	public MainMenuScreen mainMenuScreen;
+	//public PlayScreen playScreen;
+	
+	@Override
+	public void create () {
+		assets = new AssetManager();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, V_WIDTH, V_HEIGHT);
+		batch = new SpriteBatch();
+
+		initFonts();
+
+		loadingScreen = new LoadingScreen(this);
+		splashScreen = new SplashScreen(this);
+		mainMenuScreen = new MainMenuScreen(this);
+		//playScreen = new PlayScreen(this);
+
+		this.setScreen(loadingScreen);
+	}
+
+	@Override
+	public void render () {
+		super.render();
+
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		font24.dispose();
+		assets.dispose();
+		loadingScreen.dispose();
+		splashScreen.dispose();
+		mainMenuScreen.dispose();
+		//playScreen.dispose();
+	}
+
+	private void initFonts() {
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Arcon.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+		params.size = 24;
+		params.color = Color.BLACK;
+		font24 = generator.generateFont(params);
+	}
+}
